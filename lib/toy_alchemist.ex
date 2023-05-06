6e82ae698c14a11a90e5a43d24f27e2330b8f9a2
Jamie Wright <jamie@brilliantfantastic.com>
2023-05-06 13:08:10 -0400

    Change position from an Integer to a map with an east key

--- lib/toy_alchemist.ex
+++ lib/toy_alchemist.ex
@@ -10,11 +10,11 @@ defmodule ToyAlchemist do
 
   ## Examples
 
-    iex> ToyAlchemist.move_east(%Alchemist{position: 1})
-    %Alchemist{position: 2}
+    iex> ToyAlchemist.move_east(%Alchemist{position: %{east: 1}})
+    %Alchemist{position: %{east: 2}}
   """
-  def move_east(%Alchemist{position: position} = alchemist) do
-    %Alchemist{alchemist | position: position + 1}
+  def move_east(%Alchemist{position: %{east: east} = position} = alchemist) do
+    %Alchemist{alchemist | position: %{position | east: east + 1}}
   end
 
   @doc """
@@ -22,10 +22,10 @@ defmodule ToyAlchemist do
 
   ## Examples
 
-    iex> ToyAlchemist.move_west(%Alchemist{position: 1})
-    %Alchemist{position: 0}
+    iex> ToyAlchemist.move_west(%Alchemist{position: %{east: 1}})
+    %Alchemist{position: %{east: 0}}
   """
-  def move_west(%Alchemist{position: position} = alchemist) do
-    %Alchemist{alchemist | position: position - 1}
+  def move_west(%Alchemist{position: %{east: east} = position} = alchemist) do
+    %Alchemist{alchemist | position: %{position | east: east - 1}}
   end
 end
--- test/toy_alchemist_test.exs
+++ test/toy_alchemist_test.exs
@@ -6,10 +6,10 @@ defmodule ToyAlchemistTest do
   doctest ToyAlchemist
 
   describe "move_east/1" do
-    test "increments the x-axis of the alchemist" do
+    test "increments the east position of the alchemist" do
       alchemist = Alchemist.new(0)
 
-      assert ToyAlchemist.move_east(alchemist) == %Alchemist{position: 1}
+      assert ToyAlchemist.move_east(alchemist) == %Alchemist{position: %{east: 1}}
     end
 
     test "chaining incrementing the east position" do
@@ -19,25 +19,25 @@ defmodule ToyAlchemistTest do
         |> ToyAlchemist.move_east()
         |> ToyAlchemist.move_east()
 
-      assert alchemist == %Alchemist{position: 4}
+      assert alchemist == %Alchemist{position: %{east: 4}}
     end
   end
 
   describe "move_west/1" do
-    test "decrements the x-axis of the alchemist" do
+    test "decrements the east position of the alchemist" do
       alchemist = Alchemist.new(0)
 
-      assert ToyAlchemist.move_west(alchemist) == %Alchemist{position: -1}
+      assert ToyAlchemist.move_west(alchemist) == %Alchemist{position: %{east: -1}}
     end
 
-    test "chaining incrementing the west position" do
+    test "chaining decrementing the east position" do
       alchemist =
         Alchemist.new(-2)
         |> ToyAlchemist.move_west()
         |> ToyAlchemist.move_west()
         |> ToyAlchemist.move_west()
 
-      assert alchemist == %Alchemist{position: -5}
+      assert alchemist == %Alchemist{position: %{east: -5}}
     end
   end
 end
