bcce23b66759d49a3f6d06b95cc3d0d247096d2f
Jamie Wright <jamie@brilliantfantastic.com>
2023-05-06 14:04:48 -0400

    Introduce the position struct

--- lib/toy_alchemist/position.ex
+++ lib/toy_alchemist/position.ex
@@ -4,4 +4,52 @@ defmodule ToyAlchemist.Position do
   def new(north, east) do
     struct!(__MODULE__, %{north: north, east: east})
   end
+
+  @doc """
+  Moves a Position` one space in the east direction.
+
+  ## Examples
+
+    iex> Position.move_east(%Position{east: 1})
+    %Position{east: 2}
+  """
+  def move_east(%__MODULE__{east: east} = position) do
+    %__MODULE__{position | east: east + 1}
+  end
+
+  @doc """
+  Moves a Position` one space in the north direction.
+
+  ## Examples
+
+    iex> Position.move_north(%Position{north: 2})
+    %Position{north: 3}
+  """
+  def move_north(%__MODULE__{north: north} = position) do
+    %__MODULE__{position | north: north + 1}
+  end
+
+  @doc """
+  Moves a Position` one space in the south direction.
+
+  ## Examples
+
+    iex> Position.move_south(%Position{north: 2})
+    %Position{north: 1}
+  """
+  def move_south(%__MODULE__{north: north} = position) do
+    %__MODULE__{position | north: north - 1}
+  end
+
+  @doc """
+  Moves a Position` one space in the west direction.
+
+  ## Examples
+
+    iex> Position.move_west(%Position{east: -3})
+    %Position{east: -4}
+  """
+  def move_west(%__MODULE__{east: east} = position) do
+    %__MODULE__{position | east: east - 1}
+  end
 end
--- test/toy_alchemist/position_test.exs
+++ test/toy_alchemist/position_test.exs
@@ -10,4 +10,28 @@ defmodule ToyAlchemist.PositionTest do
       assert Position.new(2, 3) == %Position{north: 2, east: 3}
     end
   end
+
+  describe "move_east/1" do
+    test "increments the east position of the position" do
+      position = Position.new(0, 0)
+
+      assert Position.move_east(position) == %Position{east: 1, north: 0}
+    end
+  end
+
+  describe "move_north/1" do
+    test "increments the north position of the position" do
+      position = Position.new(0, 0)
+
+      assert Position.move_north(position) == %Position{east: 0, north: 1}
+    end
+  end
+
+  describe "move_west/1" do
+    test "decrements the east position of the position" do
+      position = Position.new(0, 0)
+
+      assert Position.move_west(position) == %Position{east: -1, north: 0}
+    end
+  end
 end
