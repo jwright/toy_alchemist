a2d4cb575fe8361cb4cec5805645b49fa06d3ee1
Jamie Wright <jamie@brilliantfantastic.com>
2023-05-08 18:01:24 -0400

    Add a predicate function to check in the position is valid

--- lib/toy_alchemist/table.ex
+++ lib/toy_alchemist/table.ex
@@ -4,4 +4,15 @@ defmodule ToyAlchemist.Table do
   def new(north_boundary, east_boundary) do
     struct!(__MODULE__, north_boundary: north_boundary, east_boundary: east_boundary)
   end
+
+  def valid_position?(
+        %__MODULE__{north_boundary: north_boundary, east_boundary: east_boundary},
+        north_position,
+        east_position
+      ) do
+    within_boundary?(north_boundary, north_position) &&
+      within_boundary?(east_boundary, east_position)
+  end
+
+  defp within_boundary?(boundary, position), do: 0..boundary |> Enum.member?(position)
 end
