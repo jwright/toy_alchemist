891beaba2ca8b4e79d3bd426c224283abed1baa6
Jamie Wright <jamie@brilliantfantastic.com>
2023-05-08 20:08:06 -0400

    Move functions onto Alchemist

--- lib/toy_alchemist/alchemist.ex
+++ lib/toy_alchemist/alchemist.ex
@@ -13,11 +13,83 @@ defmodule ToyAlchemist.Alchemist do
   ## Examples
 
     iex> Alchemist.new(3, 4, facing: :south)
-    %Alchemist{%Position{north: 3, east: 4, south: -3, west: -4}, facing: :south}
+    %Alchemist{position: %ToyAlchemist.Position{north: 3, east: 4, south: -3, west: -4}, facing: :south}
   """
   def new(north \\ 0, east \\ 0, opts \\ []) do
     facing = Keyword.get(opts, :facing, :north)
 
     struct!(__MODULE__, facing: facing, position: Position.new(north, east))
+  end
+
+  @doc """
+  Moves an `Alchemist` one space in the direction the alchemist is currently facing.
+
+  ## Examples
+
+    iex> Alchemist.move(%Alchemist{position: %Position{east: 2, north: 2, south: -2, west: -2}, facing: :east})
+    %Alchemist{position: %Position{east: 3, north: 2, west: -3, south: -2}, facing: :east}
+  """
+  def move(%__MODULE__{facing: facing} = alchemist) do
+    case facing do
+      :north -> alchemist |> move_north()
+      :east -> alchemist |> move_east()
+      :south -> alchemist |> move_south()
+      :west -> alchemist |> move_west()
+      _ -> alchemist
+    end
+  end
+
+  defp move_east(%__MODULE__{position: position} = alchemist) do
+    %__MODULE__{alchemist | position: Position.move_east(position)}
+  end
+
+  defp move_north(%__MODULE__{position: position} = alchemist) do
+    %__MODULE__{alchemist | position: Position.move_north(position)}
+  end
+
+  defp move_south(%__MODULE__{position: position} = alchemist) do
+    %__MODULE__{alchemist | position: Position.move_south(position)}
+  end
+
+  defp move_west(%__MODULE__{position: position} = alchemist) do
+    %__MODULE__{alchemist | position: Position.move_west(position)}
+  end
+
+  @doc """
+  Turns an `Alchemist` one direction to the left and updates the facing direction to
+  the new direction it is using.
+
+  ## Examples
+
+    iex> Alchemist.turn_left(%Alchemist{facing: :east})
+    %Alchemist{facing: :north}
+  """
+  def turn_left(%__MODULE__{facing: facing} = alchemist) do
+    case facing do
+      :north -> %__MODULE__{alchemist | facing: :west}
+      :east -> %__MODULE__{alchemist | facing: :north}
+      :south -> %__MODULE__{alchemist | facing: :east}
+      :west -> %__MODULE__{alchemist | facing: :south}
+      _ -> alchemist
+    end
+  end
+
+  @doc """
+  Turns an `Alchemist` one direction to the right and updates the facing direction to
+  the new direction it is using.
+
+  ## Examples
+
+    iex> Alchemist.turn_right(%Alchemist{facing: :east})
+    %Alchemist{facing: :south}
+  """
+  def turn_right(%__MODULE__{facing: facing} = alchemist) do
+    case facing do
+      :north -> %__MODULE__{alchemist | facing: :east}
+      :east -> %__MODULE__{alchemist | facing: :south}
+      :south -> %__MODULE__{alchemist | facing: :west}
+      :west -> %__MODULE__{alchemist | facing: :north}
+      _ -> alchemist
+    end
   end
 end
--- test/toy_alchemist/alchemist_test.exs
+++ test/toy_alchemist/alchemist_test.exs
@@ -1,7 +1,9 @@
 defmodule ToyAlchemist.AlchemistTest do
   use ExUnit.Case
 
-  alias ToyAlchemist.Alchemist
+  alias ToyAlchemist.{Alchemist, Position}
+
+  doctest Alchemist
 
   describe "new/1" do
     test "returns an Alchemist with defaults" do
@@ -13,6 +15,156 @@ defmodule ToyAlchemist.AlchemistTest do
 
     test "allows to specify the facing direction" do
       assert Alchemist.new(0, 0, facing: :south).facing == :south
+    end
+  end
+
+  describe "move/1" do
+    test "when facining east, increments the east position" do
+      alchemist = Alchemist.new(0, 0, facing: :east) |> Alchemist.move()
+
+      assert alchemist.position == %Position{east: 1, west: -1, north: 0, south: 0}
+    end
+
+    test "when facining north, increments the north position" do
+      alchemist = Alchemist.new(0, 0, facing: :north) |> Alchemist.move()
+
+      assert alchemist.position == %Position{east: 0, west: 0, north: 1, south: -1}
+    end
+
+    test "when facining south, increments the south position" do
+      alchemist = Alchemist.new(0, 0, facing: :south) |> Alchemist.move()
+
+      assert alchemist.position == %Position{east: 0, west: 0, north: -1, south: 1}
+    end
+
+    test "when facining west, increments the west position" do
+      alchemist = Alchemist.new(0, 0, facing: :west) |> Alchemist.move()
+
+      assert alchemist.position == %Position{east: -1, west: 1, north: 0, south: 0}
+    end
+
+    test "when facing a weird direction, it does not change the position" do
+      alchemist = Alchemist.new(1, 2, facing: :diagonal) |> Alchemist.move()
+
+      assert alchemist.position == %Position{east: 2, west: -2, north: 1, south: -1}
+    end
+
+    test "chaining moves" do
+      alchemist =
+        Alchemist.new(0, 1, facing: :east)
+        |> Alchemist.move()
+        |> Alchemist.move()
+        |> Alchemist.move()
+
+      assert alchemist.position == %Position{east: 4, west: -4, north: 0, south: 0}
+    end
+  end
+
+  describe "turn_left/1" do
+    test "when facining east, it now faces north" do
+      alchemist = Alchemist.new(0, 0, facing: :east) |> Alchemist.turn_left()
+
+      assert alchemist.facing == :north
+    end
+
+    test "when facining north, it now faces west" do
+      alchemist = Alchemist.new(0, 0, facing: :north) |> Alchemist.turn_left()
+
+      assert alchemist.facing == :west
+    end
+
+    test "when facining west, it now faces south" do
+      alchemist = Alchemist.new(0, 0, facing: :west) |> Alchemist.turn_left()
+
+      assert alchemist.facing == :south
+    end
+
+    test "when facining south, it now faces east" do
+      alchemist = Alchemist.new(0, 0, facing: :south) |> Alchemist.turn_left()
+
+      assert alchemist.facing == :east
+    end
+
+    test "when facing a weird direction, it does not change the direction" do
+      alchemist = Alchemist.new(1, 2, facing: :diagonal) |> Alchemist.turn_left()
+
+      assert alchemist.facing == :diagonal
+    end
+
+    test "chaining turns" do
+      alchemist =
+        Alchemist.new(0, 1, facing: :east)
+        |> Alchemist.turn_left()
+        |> Alchemist.turn_left()
+        |> Alchemist.turn_left()
+
+      assert alchemist.facing == :south
+    end
+  end
+
+  describe "turn_right/1" do
+    test "when facining east, it now faces south" do
+      alchemist = Alchemist.new(0, 0, facing: :east) |> Alchemist.turn_right()
+
+      assert alchemist.facing == :south
+    end
+
+    test "when facining north, it now faces east" do
+      alchemist = Alchemist.new(0, 0, facing: :north) |> Alchemist.turn_right()
+
+      assert alchemist.facing == :east
+    end
+
+    test "when facining west, it now faces north" do
+      alchemist = Alchemist.new(0, 0, facing: :west) |> Alchemist.turn_right()
+
+      assert alchemist.facing == :north
+    end
+
+    test "when facining south, it now faces west" do
+      alchemist = Alchemist.new(0, 0, facing: :south) |> Alchemist.turn_right()
+
+      assert alchemist.facing == :west
+    end
+
+    test "when facing a weird direction, it does not change the direction" do
+      alchemist = Alchemist.new(1, 2, facing: :diagonal) |> Alchemist.turn_right()
+
+      assert alchemist.facing == :diagonal
+    end
+
+    test "chaining turns" do
+      alchemist =
+        Alchemist.new(0, 1, facing: :east)
+        |> Alchemist.turn_right()
+        |> Alchemist.turn_right()
+        |> Alchemist.turn_right()
+
+      assert alchemist.facing == :north
+    end
+  end
+
+  describe "chaining movement" do
+    test "ends up in the correct location and facing the correct direction" do
+      alchemist =
+        Alchemist.new(0, 0, facing: :north)
+        |> Alchemist.move()
+        |> Alchemist.turn_right()
+        |> Alchemist.move()
+        |> Alchemist.turn_left()
+        |> Alchemist.move()
+        |> Alchemist.move()
+        |> Alchemist.turn_left()
+        |> Alchemist.move()
+        |> Alchemist.turn_right()
+        |> Alchemist.turn_right()
+        |> Alchemist.move()
+        |> Alchemist.move()
+        |> Alchemist.move()
+
+      assert alchemist.position.north == 3
+      assert alchemist.position.east == 3
+      assert alchemist.facing == :east
     end
   end
 end
