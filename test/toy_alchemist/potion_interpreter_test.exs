bd04094d9ef02a590d2d2426c052f8a0aa654743
Jamie Wright <jamie@brilliantfantastic.com>
2023-05-09 00:46:06 -0400

    Add a test for multiple potions

--- test/toy_alchemist/potion_interpreter_test.exs
+++ test/toy_alchemist/potion_interpreter_test.exs
@@ -35,5 +35,21 @@ defmodule ToyAlchemist.PotionInterpreterTest do
     test "with an invalid potion, returns an invalid tuple" do
       assert PotionInterpreter.interpret(["BLAH"]) == [{:invalid, "BLAH"}]
     end
+
+    test "interprets several potions" do
+      potions = [
+        "PLACE 1,3,EAST",
+        "MOVE",
+        "BLAH",
+        "REPORT"
+      ]
+
+      assert PotionInterpreter.interpret(potions) == [
+               {:place, [north: 1, east: 3, facing: :east]},
+               {:move},
+               {:invalid, "BLAH"},
+               {:report}
+             ]
+    end
   end
 end
